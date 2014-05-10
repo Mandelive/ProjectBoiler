f384d7a1775625fb447b8be11391d3879d710ede
Nadel Mort <mandelive@gmail.com>
2014-05-10 16:10:52 +0300

    Defined Constants and Sequences classes

---
+++ ProjectBoiler/Boilerplate/BoilConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boilerplate
+{
+    public class BoilConstants
+    {
+        public static const double phi = 1.61803398875;
+        public static const double pi = 3.14159265359;
+        public static const double e = 2.71828182846;
+    }
+}
---
+++ ProjectBoiler/Boilerplate/BoilSequences.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boilerplate
+{
+    public class BoilSequences
+    {
+        public static List<long> FibonacciSequence(long upperlimit)
+        {
+            var result = new List<long>();
+            if (upperlimit < 1)
+            {
+                return result;
+            }
+            result.Add(1L);
+            result.Add(1L);
+            if (upperlimit < 2)
+            {
+                return result;
+            }
+
+            var fib1 = 1L;
+            var fib2 = 1L;
+            var fib3 = fib1 + fib2;
+
+            result.Capacity = (int)Math.Ceiling((Math.Log(upperlimit + 0.5) + 0.5 * Math.Log(5)) / Math.Log(BoilConstants.phi)) + 5;
+
+            while (fib3 <= upperlimit)
+            {
+                result.Add(fib3);
+                fib1 = fib2;
+                fib2 = fib3;
+                fib3 += fib1;
+            }
+
+            return result;
+        }
+    }
+}
