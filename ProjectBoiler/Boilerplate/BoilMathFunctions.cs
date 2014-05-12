01fd95f2383ef9d0e9a48200ade8fb5223417dce
Nadel Mort <mandelive@gmail.com>
2014-05-12 09:34:40 +0300

    Implemented MillerRabin

--- ProjectBoiler/Boilerplate/BoilMathFunctions.cs
+++ ProjectBoiler/Boilerplate/BoilMathFunctions.cs
@@ -87,7 +87,7 @@ namespace Boilerplate
             return a << shift;
         }
 
-        public static bool IsPrimeRabinMiller(long n)
+        public static bool IsPrimeTrialDivision(long n)
         {
             if (n < 2)
             {
@@ -97,17 +97,51 @@ namespace Boilerplate
             {
                 return true;
             }
+            else if ((n & 1L) == 0)
+            {
+                return false;
+            }
+
+            var nsqrt = (long)(Math.Sqrt(n));
+            var primes = BoilSequences.PrimesSequenceUpTo(nsqrt);
+
+            foreach (var p in primes)
+            {
+                if (n % p == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool IsPrimeMillerRabin(long n)
+        {
+            if (n < 2)
+            {
+                return false;
+            }
+            else if (n < 4)
+            {
+                return true;
+            }
+            else if ((n & 1L) == 0)
+            {
+                return false;
+            }
 
             long[] witnesses;
-            if (n < 1373653)
+
+            if (n < 1373653L)
             {
                 witnesses = new long[] { 2L, 3L };
             }
-            else if (n < 4759123141)
+            else if (n < 4759123141L)
             {
                 witnesses = new long[] { 2L, 7L, 61L };
             }
-            else if (n < 47636622961201)
+            else if (n < 47636622961201L)
             {
                 witnesses = new long[] { 2L, 2570940L, 211991001L, 3749873356L };
             }
@@ -116,11 +150,47 @@ namespace Boilerplate
                 witnesses = new long[] { 2L, 2570940L, 880937L, 610386380L, 4130785767L };
             }
 
+            var s = n - 1;
+            while ((s & 1L) == 0)
+            {
+                s >>= 1;
+            }
+
             for (int i = 0; i < witnesses.Length; i++)
             {
-
+                var a = witnesses[i];
+                var temp = s;
+                var mod = (long)Math.Pow((double)a, (double)temp) % n;
+                while(temp != n - 1 && mod != 1 && mod != n - 1)
+                {
+		            mod = (mod * mod) % n;
+		            temp = temp * 2;
+                }
+                if(mod != n - 1 && temp % 2 == 0)
+                {
+		            return false;
+                }
             }
+
             return true;
+        }
+
+        public static bool IsPrimeHybrid(long n)
+        {
+            if (n < 2809)
+            {
+                return IsPrimeTrialDivision(n);
+            }
+            else if ((n & 1L) == 0)
+            {
+                return false;
+            }
+            else if (BoilMathFunctions.GcdMod(307444891294245705L, n) > 1) //pre-computed multiplciation of first few primes (3 * 5 * ... * 47)
+            {
+                return false;
+            }
+
+            return IsPrimeMillerRabin(n);
         }
     }
 }
