11fff28bff65f8c548681fd8f8b3dd009033fd0e
Nadel Mort <mandelive@gmail.com>
2018-03-22 13:01:13 +0300

    Refresh repo

---
+++ ProjectBoiler/BoiledProblems/Problem28.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Boilerplate;
+
+namespace BoiledProblems
+{
+    public class Problem28 : BaseProblem
+    {
+        public Problem28()
+        {
+            Id = 28;
+            Title = @"TnVtYmVyIHNwaXJhbCBkaWFnb25hbHM=";
+            Description = @"U3RhcnRpbmcgd2l0aCB0aGUgbnVtYmVyIDEgYW5kIG1vdmluZyB0byB0aGUgcmlnaHQgaW4gYSBjbG9ja3dpc2UgZGlyZWN0aW9uIGEgNSBieSA1IHNwaXJhbCBpcyBmb3JtZWQgYXMgZm9sbG93czoNCg0KMjEgMjIgMjMgMjQgMjUNCjIwICA3ICA4ICA5IDEwDQoxOSAgNiAgMSAgMiAxMQ0KMTggIDUgIDQgIDMgMTINCjE3IDE2IDE1IDE0IDEzDQoNCkl0IGNhbiBiZSB2ZXJpZmllZCB0aGF0IHRoZSBzdW0gb2YgdGhlIG51bWJlcnMgb24gdGhlIGRpYWdvbmFscyBpcyAxMDEuDQoNCldoYXQgaXMgdGhlIHN1bSBvZiB0aGUgbnVtYmVycyBvbiB0aGUgZGlhZ29uYWxzIGluIGEgMTAwMSBieSAxMDAxIHNwaXJhbCBmb3JtZWQgaW4gdGhlIHNhbWUgd2F5Pw==";
+
+            parametersInfo = new string[]
+            {
+                "n:num - size of the spiral",
+            };
+
+            defaultParameters = new string[]
+            {
+                "1001"
+            };
+
+            ResetParameters();
+        }
+
+        public override string Solve()
+        {
+            var n = Int32.Parse(parameters[0]);
+            return findSpiralNumbersDiagonalSum(n).ToString();
+        }
+
+        private long findSpiralNumbersDiagonalSum(int n)
+        {
+            var result = 1L;
+
+            for (int i = 3; i <= n; i += 2)
+            {
+                result += 4 * (i * i) - (6 * (i - 1));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProjectBoiler/BoiledProblems/Problem29.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Boilerplate;
+
+namespace BoiledProblems
+{
+    public class Problem29 : BaseProblem
+    {
+        public Problem29()
+        {
+            Id = 29;
+            Title = @"RGlzdGluY3QgcG93ZXJz";
+            Description = @"Q29uc2lkZXIgYWxsIGludGVnZXIgY29tYmluYXRpb25zIG9mIGFeYiBmb3IgMiDiiaQgYSDiiaQgNSBhbmQgMiDiiaQgYiDiiaQgNToNCg0KMl4yPTQsIDJeMz04LCAyXjQ9MTYsIDJeNT0zMg0KM14yPTksIDNeMz0yNywgM140PTgxLCAzXjU9MjQzDQo0XjI9MTYsIDReMz02NCwgNF40PTI1NiwgNF41PTEwMjQNCjVeMj0yNSwgNV4zPTEyNSwgNV40PTYyNSwgNV41PTMxMjUNCg0KSWYgdGhleSBhcmUgdGhlbiBwbGFjZWQgaW4gbnVtZXJpY2FsIG9yZGVyLCB3aXRoIGFueSByZXBlYXRzIHJlbW92ZWQsIHdlIGdldCB0aGUgZm9sbG93aW5nIHNlcXVlbmNlIG9mIDE1IGRpc3RpbmN0IHRlcm1zOg0KDQo0LCA4LCA5LCAxNiwgMjUsIDI3LCAzMiwgNjQsIDgxLCAxMjUsIDI0MywgMjU2LCA2MjUsIDEwMjQsIDMxMjUNCg0KSG93IG1hbnkgZGlzdGluY3QgdGVybXMgYXJlIGluIHRoZSBzZXF1ZW5jZSBnZW5lcmF0ZWQgYnkgYV5iIGZvciAyIOKJpCBhIOKJpCAxMDAgYW5kIDIg4omkIGIg4omkIDEwMD8=";
+
+            parametersInfo = new string[]
+            {
+                "a:num - range for base a",
+                "b:num - range for exponent b",
+            };
+
+            defaultParameters = new string[]
+            {
+                "100",
+                "100"
+            };
+
+            ResetParameters();
+        }
+
+        public override string Solve()
+        {
+            var a = Int32.Parse(parameters[0]);
+            var b = Int32.Parse(parameters[1]);
+            return findDistinctPowers(a, b).ToString();
+        }
+
+        private long findDistinctPowers(int a, int b)
+        {
+            long result = (a - 1) * (b - 1);
+
+            var covered = new bool[a + 1];
+
+            var repsDict = new Dictionary<int, bool[]>();
+
+            for (int i = 2; i <= a; i++)
+            {
+                if (covered[i])
+                {
+                    continue;
+                }
+
+                var generation = 2;
+                var genBase = (int)Math.Pow(i, generation);
+                while (genBase <= a)
+                {
+                    covered[genBase] = true;
+                    repsDict.Add(genBase, new bool[b + 1]);
+
+                    for (int j = 2; j < generation; j++)
+                    {
+                        var start = 0;
+                        var end = 0;
+                        var increment = (int)BoilMathFunctions.LcmGcd(BoilMathFunctions.ModPow(i, j, 1), genBase);
+                        for (int k = start; k  <= end; k += increment)
+                        {
+                            repsDict[genBase][k] = true;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
